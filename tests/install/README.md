d3229aee69fbab493bd6a08bf927594a821f2c0a
SenpuDev <senpuudev@gmail.com>
2023-09-12 13:58:40 +0200

    wp install testing

--- tests/install/README.md
+++ tests/install/README.md
@@ -1,6 +1,5 @@
 # How to install it?
 - Open your terminal and navigate to this directory.
-- Make sure that your docker is ready to up.
 - Execute: docker-compose up -d
 - Now, you can open a new tab on your explorer to: http://localhost:8080 A wordpress default page should be displayed.
 
@@ -8,9 +7,3 @@
 
 - user: root
 - password: root
-
-## Quality of life if you are getting "lower_case_table_names settings for server ('2') and data dictionary ('0')" ERROR on Windows
-
-- Go to ./mysql_data directory.
-- Execute in terminal: fsutil.exe file queryCaseSensitiveInfo ./ 
-- If case sensitive is disabled, enable with the following command: fsutil.exe file setCaseSensitiveInfo ./ enable 
--- tests/install/start.sh
+++ tests/install/start.sh
@@ -1,13 +1,38 @@
 #!/bin/sh
 
-# wget & unzip needed to download and extract plugin
+# # wget & unzip needed to download and extract plugin
 apt-get update
-apt-get install -y wget unzip
+apt-get install -y wget unzip default-mysql-client
 
-# Download and extract plugin
+echo 'Command packages installed'
+
+# Copiar los archivos de WordPress
+cp -r /usr/src/wordpress/. /var/www/html/
+
+# Copiar el archivo wp-config.php personalizado
+cp /usr/local/bin/wp-config.php /var/www/html/wp-config.php
+
+echo 'Wp initial config ready'
+
+while true; do
+  if mysqladmin ping -h "database" -u "root" -pmypassword > /dev/null 2>&1; then
+    echo "Database is ready..."
+    break
+  else
+    echo "Waiting for Database initial config..."
+    sleep 5
+  fi
+done
+
+# Extract data from backup.sql to the database (default site, user:root, pass:root...)
+mysql -h database -u root -pmypassword wordpress < /usr/local/bin/backup.sql
+
+echo 'backup.sql successfully restored'
+
+# Download and extract plugin (Api Management)
 wget https://github.com/granalacant/wp-azure-api-management/archive/refs/heads/main.zip -O /tmp/main.zip
 unzip -o /tmp/main.zip -d /var/www/html/wp-content/plugins/
-rm /tmp/main.zip
+rm /tmp/main.zip # Removes .zip
 
 # Init wordpress
 exec docker-entrypoint.sh apache2-foreground
